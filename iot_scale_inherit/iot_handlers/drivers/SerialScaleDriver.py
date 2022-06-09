ba1bb96d09e68ac857d969cabfa72260d3c1253d
Pinakin Nayi <odoo-erp@Odoo-ERPs-MacBook-Air.local>
2022-06-09 16:23:49 +0530

    fixes

---
+++ iot_scale_inherit/iot_handlers/drivers/SerialScaleDriver.py
@@ -0,0 +1,9 @@
+# -- coding: utf-8 --
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# from odoo.addons.hw_drivers.iot_handlers.drivers import SerialScaleDriver
+from odoo.addons.hw_drivers.iot_handlers.drivers.SerialScaleDriver import AdamEquipmentDriver
+
+AdamEquipmentDriver._protocol=AdamEquipmentDriver._protocol._replace(baudrate=9600)
+
+print("+++++++++++++:",AdamEquipmentDriver._protocol)
