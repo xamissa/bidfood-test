cc6cd449139776bafe0b3bce8fae919836e22248
Pinakin Nayi <odoo-erp@Odoo-ERPs-MacBook-Air.local>
2022-08-16 10:41:46 +0530

    fix

--- custom_iot/models/SerialScaleDriver.py
+++ custom_iot/models/SerialScaleDriver.py
@@ -2,12 +2,12 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 
-from odoo.addons.hw_drivers.iot_handlers.drivers.SerialScaleDriver import ADAMEquipmentProtocol, ScaleDriver
+from odoo.addons.hw_drivers.iot_handlers.drivers.SerialScaleDriver import ADAMEquipmentProtocol#, ScaleDriver
 
 ADAMEquipmentProtocol.baudrate = 9600
 
-class AdamEquipmentDriver(ScaleDriver):
-    _protocol = ADAMEquipmentProtocol
+# class AdamEquipmentDriver(ScaleDriver):
+#     _protocol = ADAMEquipmentProtocol
 
-    def __init__(self, identifier, device):
-        super.__init__(identifier, device)
+#     def __init__(self, identifier, device):
+#         super.__init__(identifier, device)
