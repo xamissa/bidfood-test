f7bcb7ab6c8849dd22c2ed46c052c3d5a4638129
Erpweb8 <nikita@erpweb.co.za>
2024-04-10 10:22:17 +0530

    Product label print - VAT Inclusive price#17198

---
+++ bidfood_product_label/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'BidFood Product Label',
+    'version': '1.0 ',
+    'category': 'Product',
+    'sequence': 6,
+    'summary': 'Add the VAT included price on print label',
+    'description': '',
+    'data': [
+        'views/product_views.xml',
+    ],
+    'depends': ['product', 'account'],
+    'qweb': [''],
+    'installable': True,
+    'license': 'LGPL-3',
+}
---
+++ bidfood_product_label/models/product.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models, api, _
+from odoo.tools import format_amount
+import logging
+_logger = logging.getLogger(__name__)
+import re
+
+class Productemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.depends('taxes_id', 'list_price')
+    def get_tax_price(self):
+        for rec in self:
+            currency = rec.currency_id
+            amt = 0
+            res = rec.taxes_id.compute_all(rec.list_price, product=self, partner=self.env['res.partner'])
+            included = res['total_included']
+            if currency.compare_amounts(included, rec.list_price):
+                amt = format_amount(self.env, included, currency)
+            excluded = res['total_excluded']
+            if currency.compare_amounts(excluded, rec.list_price):
+                amt = format_amount(self.env, excluded, currency)
+            return amt
+
+    @api.depends('taxes_id', 'list_price')
+    def get_tax_price_zpl(self):
+        for rec in self:
+            currency = rec.currency_id
+            amt = 0
+            res = rec.taxes_id.compute_all(rec.list_price, product=self, partner=self.env['res.partner'])
+            included = res['total_included']
+            if currency.compare_amounts(included, rec.list_price):
+                amt = format_amount(self.env, included, currency)
+            excluded = res['total_excluded']
+            if currency.compare_amounts(excluded, rec.list_price):
+                amt = format_amount(self.env, excluded, currency)
+
+            trim = re.compile(r'[^\d.,]+')
+            amt = trim.sub('', amt)
+            return float(amt)
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.depends('lst_price', 'taxes_id')
+    def get_tax_price(self):
+        for rec in self:
+            currency = rec.currency_id
+            amt = 0
+            res = rec.taxes_id.compute_all(rec.lst_price, product=self, partner=self.env['res.partner'])
+            included = res['total_included']
+            if currency.compare_amounts(included, rec.lst_price):
+                amt = format_amount(self.env, included, currency)
+            excluded = res['total_excluded']
+            if currency.compare_amounts(excluded, rec.lst_price):
+                amt = format_amount(self.env, excluded, currency)
+            return amt
+
+    @api.depends('lst_price', 'taxes_id')
+    def get_tax_price_zpl(self):
+        for rec in self:
+            currency = rec.currency_id
+            amt = 0
+            res = rec.taxes_id.compute_all(rec.lst_price, product=self, partner=self.env['res.partner'])
+            included = res['total_included']
+            if currency.compare_amounts(included, rec.lst_price):
+                amt = format_amount(self.env, included, currency)
+            excluded = res['total_excluded']
+            if currency.compare_amounts(excluded, rec.lst_price):
+                amt = format_amount(self.env, excluded, currency)
+
+            trim = re.compile(r'[^\d.,]+')
+            amt = trim.sub('', amt)
+            return float(amt)
