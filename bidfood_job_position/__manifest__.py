7c2cb7a076a84a7989e9eeac616897e01e860ebc
Nikhil <nikhil@erpweb.co.za>
2024-11-19 15:11:43 +0530

    POS - Marketing (Job Position)#21270

---
+++ bidfood_job_position/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "BidFood Job Position",
+    "summary": """ Job Position""",
+    "images": [],
+    "version": "15.0.1",
+    "depends": ["base", "point_of_sale", "contacts"],
+    "data": [
+        'security/ir.model.access.csv',
+        'views/job_position.xml',
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'bidfood_job_position/static/src/js/models.js',
+            'bidfood_job_position/static/src/js/ClientDetailsEdit.js',
+        ],
+        'web.assets_qweb': [
+            'bidfood_job_position/static/src/xml/*',
+        ],
+    },  
+    "auto_install": True,
+    "installable": True,
+    "license" : "OPL-1",
+}
