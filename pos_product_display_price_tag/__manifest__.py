cc6fce09a620b75413337afe96c02d130f113c07
ERPWEB - Nikhil <nikhil@erpweb.co.za>
2026-06-01 11:38:49 +0530

    Add Price tag with product in point of sale

---
+++ pos_product_display_price_tag/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "PoS Product Display Internal Reference",
+    "summary": "Display Internal Reference with product name",
+    "version": "19.0",
+    "category": "Point of Sale",
+    "website": "",
+    "author": "",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "point_of_sale",
+    ],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'pos_product_display_price_tag/static/src/app/generic_components/product_card/product_card.xml',
+            
+        ],
+    },
+    "demo": [],
+    "qweb": [],
+}
