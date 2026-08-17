d45e0cd3a0958eece27b24a97256677362285976
ERPWEB - Nikhil <nikhil@erpweb.co.za>
2026-08-17 15:00:03 +0530

    Fix issue after migration

--- custom_receipts_for_pos/models/pos_session.py
+++ custom_receipts_for_pos/models/pos_session.py
@@ -4,3 +4,12 @@ from odoo import models
 
 class PosSession(models.Model):
     _inherit = 'pos.session'
+
+    def _load_pos_data_models(self, config):
+        """Add pos.receipt to the POS UI data load."""
+        models = super()._load_pos_data_models(config)
+
+        if 'pos.receipt' not in models:
+            models.append('pos.receipt')
+
+        return models
